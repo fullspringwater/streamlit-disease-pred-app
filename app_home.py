5fdc67eb9baad16f269ff6fd97914fc982e4353a
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-26 14:20:52 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def run_home() :
     st.header('EDA')
-    st.subheader('데이터 시각화를 한 기능입니다.')
+    st.subheader('데이터 시각화')
 
     st.header('Disease Pred')
-    st.subheader('증상으로 질병을 예측할 수 있는 기능입니다.')
+    st.subheader('증상으로 질병을 예측하기')
