78a2a229d4dae6d4c95d28fe9f80d242a5f22f43
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-26 12:54:36 +0900

    update

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 from app_disease import run_disease
 from app_eda import run_eda
+from app_home import run_home
 
 def main() :
+    st.title('증상으로 질병 예측하기 프로젝트')
     menu = ['Home', 'EDA', 'Disease Pred']
     choice_menu = st.sidebar.selectbox('메뉴 선택', menu)
 
     if choice_menu == menu[0] :
-        pass
+        run_home()
     elif choice_menu == menu[1] :
         run_eda()
     elif choice_menu == menu[2] :
